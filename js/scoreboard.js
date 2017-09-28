065ca656676055282aedb166ed59147fee14a8f7
mfullford <marissafullford@gmail.com>
2017-09-28 14:28:36 -0600

    Adds scoreboard page

---
+++ js/scoreboard.js
@@ -0,0 +1,12 @@
+//function scoreboard() {
+    // link to some array???
+   // var x = JSON.parse(localStorage.VARIABLE || '[]');
+    //var y = document.getElementById("counter");
+    //scoreboard.innerHTML = '';
+    // Appends all the scores as list elements
+    //for (var i = 0; i < score.length; i++) {
+        //var x = document.createElement('li');
+        //li.textContent = pontos[i];
+        //y.appendChild(y);
+    //}
+//}
