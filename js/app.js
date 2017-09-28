858fe50b334aef7e38034a26587448c3353ed9cd
mfullford <marissafullford@gmail.com>
2017-09-28 17:01:46 -0600

    Adds a ton of comments

--- js/app.js
+++ js/app.js
@@ -1,8 +1,152 @@
 $(document).ready(function() {
 
+
+		// Creates an image for the cursor
 	$("body").css('cursor', 'url(images/hand.png) 6 6, auto');
 
+		// To randomize the puppies I used math random and math round
+		function random(min,max){
+ 	 		return Math.round(Math.random() * (max-min) + min);
+ 		}
 
+ 		//Giving us the two puppy options
+
+		function setBG(){
+			if (Math.round(Math.random())){
+				return "http://www.pholody.com/img/running_dog.png";
+			 } else {
+ 	    		return "https://s-media-cache-ak0.pinimg.com/originals/30/82/d0/3082d0250b91ff5dc176160101f744e4.jpg";																								  } 
+			 	}	
+		// Dropping puppies everywhere	
+
+		function dropBox(){																								
+			var length = random(100, ($(".game").width() - 100));
+			var velocity = random(8000, 12000);
+			var size = random(100, 120);
+			var thisBox = $("<div/>", {
+			class: "box",
+			style:  "width:" +size+ "px; height:"+size+"px; left:" + length+  "px; transition: transform " +velocity+ "ms linear;"
+ 			});
+
+	  
+		//Set/css the data
+		thisBox.data("test", Math.round(Math.random()));
+			if(thisBox.data("test")){
+				thisBox.css({"background": "url('http://www.pholody.com/img/running_dog.png')", "background-size":"contain", "background-repeat":"no-repeat"});
+			} else {
+				thisBox.css({"background": "url('https://s-media-cache-ak0.pinimg.com/originals/30/82/d0/3082d0250b91ff5dc176160101f744e4.jpg')", "background-size":"contain", "background-repeat":"no-repeat"});
+			} 
+	  
+		//CUE PUPPIES
+		$(".game").append(thisBox);
+  
+		//Start for animation
+		setTimeout(function(){
+		thisBox.addClass("move");
+		}, random(0, 6000) );
+  
+		//Remove this object when you click
+		thisBox.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
+               function(event) {
+    			$(this).remove();
+	  			});
+				
+		}
+		
+		for (i = 0; i < 10; i++) { 
+			dropBox();
+		}
+		// On click, add exponentially to the score
+		var currentScore = 0;
+
+		$(document).on('click', '.box', function(){
+  			if($(this).data("test")){
+				currentScore++;
+			} else {
+				currentScore++;
+			}
+   			$(".score").html(currentScore);
+  			$(this).remove();
+ 		});
+
+		var runGame = setInterval(function(){
+                 for (i = 0; i < 10; i++) { 
+                  dropBox();
+                }  
+              }, 6000);
+
+	// basic timer layout
+		// 	function startTimer(duration, display) {
+		// 	    var timer = duration, minutes, seconds;
+		// 	    setInterval(function () {
+		// 	        minutes = parseInt(timer / 60, 10);
+		// 	        seconds = parseInt(timer % 60, 10);
+
+		// 	        minutes = minutes < 10 ? "0" + minutes : minutes;
+		// 	        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		// 	        display.text(minutes + ":" + seconds);
+		// 	        	// need to cap it at 30 secs
+		// 	        if (--timer < 0) {
+		// 	        	alert ("Sorry out of time! Next player, you're up!")
+		// // 	        	if(window.prompt('Out of time! Check out the high scores at','the Scoreboard Page'))
+  // // location.href='https://mfullford.github.io/project1/scoreBoard.html';
+		// 	            timer = duration;
+		// 	        }
+		// 	    }, 1000);
+		// 	}
+		// 		// 30 seconds per round
+		// 	jQuery(function ($) {
+		// 	    var thirtySeconds = 30 * 1,
+		// 	        display = $('#counter');
+		// 	        console.log("hey this is counting")
+		// 	    startTimer(thirtySeconds, display);
+		// 	});
+
+
+		// Created a form for users to submit their high scores
+
+		$(document).ready(function() {
+			$("div#form1").append(
+				// Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
+				$("<h3/>").text("Player Info"), $("<p/>").text("Fill out the info bellow to save your high score!"), $("<form/>", {
+				action: '#',
+				method: '#'
+				}).append(
+				// Create <form> Tag and Appending in HTML Div form1.
+				$("<input/>", {
+				type: 'text',
+				id: 'vname',
+				name: 'name',
+				placeholder: 'Your Name'
+				}), // Creating Input Element With Attribute.
+				$("<input/>", {
+				type: 'text',
+				id: 'vemail',
+				name: 'email',
+				placeholder: 'Your Highscore'
+				}), $("<br/>"), $("<input/>", {
+				type: 'submit',
+				id: 'submit',
+				value: 'Submit'
+			})))
+		});
+
+		// The timer resets automatically, but the score doesn't so I needed to add a restart button
+		var reset = document.querySelector('.reset');
+		 	console.log(reset);
+		 	function resetGame () {
+		  	reset.addEventListener("click", function() {
+		    location.reload();
+		    // alert("clicked reset button");
+		  });
+		  
+		 }
+		 resetGame();
+
+});
+
+// The Graveyard 
 
  //  THIS WAS MY OG collision detection but I couldn't figure it out): create a container and a box to move around 
 	// var game = $('#game'),
@@ -31,105 +175,6 @@ $(document).ready(function() {
 	//         }
 	//     });
 	// }, 20);
-
-
-		function random(min,max){
- 	 		return Math.round(Math.random() * (max-min) + min);
- 		}
-
- 		//
-
-		function setBG(){
-			if (Math.round(Math.random())){
-				return "http://www.pholody.com/img/running_dog.png";
-			 } else {
- 	    		return "https://s-media-cache-ak0.pinimg.com/originals/30/82/d0/3082d0250b91ff5dc176160101f744e4.jpg";																								  } 
-			 	}	
-	// dropping puppies everywhere	
-
-		function dropBox(){																								
-			var length = random(100, ($(".game").width() - 100));
-			var velocity = random(8000, 12000);
-			var size = random(100, 120);
-			var thisBox = $("<div/>", {
-			class: "box",
-			style:  "width:" +size+ "px; height:"+size+"px; left:" + length+  "px; transition: transform " +velocity+ "ms linear;"
- 			});
-
-	  
-//have to set/css the data
-		thisBox.data("test", Math.round(Math.random()));
-			if(thisBox.data("test")){
-				thisBox.css({"background": "url('http://www.pholody.com/img/running_dog.png')", "background-size":"contain", "background-repeat":"no-repeat"});
-			} else {
-				thisBox.css({"background": "url('https://s-media-cache-ak0.pinimg.com/originals/30/82/d0/3082d0250b91ff5dc176160101f744e4.jpg')", "background-size":"contain", "background-repeat":"no-repeat"});
-			} 
-	  
-//CUE PUPPIES
-		$(".game").append(thisBox);
-  
-//random start for animation
-		setTimeout(function(){
-		thisBox.addClass("move");
-		}, random(0, 6000) );
-  
-//remove this object when animation is over
-		thisBox.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
-               function(event) {
-    			$(this).remove();
-	  			});
-				
-		}
-		
-		for (i = 0; i < 10; i++) { 
-			dropBox();
-		}
-
-		var currentScore = 0;
-
-		$(document).on('click', '.box', function(){
-  			if($(this).data("test")){
-				currentScore++;
-			} else {
-				currentScore++;
-			}
-   			$(".score").html(currentScore);
-  			$(this).remove();
- 		});
-
-		var runGame = setInterval(function(){
-                 for (i = 0; i < 10; i++) { 
-                  dropBox();
-                }  
-              }, 6000);
-
-	// basic timer layout
-			function startTimer(duration, display) {
-			    var timer = duration, minutes, seconds;
-			    setInterval(function () {
-			        minutes = parseInt(timer / 60, 10);
-			        seconds = parseInt(timer % 60, 10);
-
-			        minutes = minutes < 10 ? "0" + minutes : minutes;
-			        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-			        display.text(minutes + ":" + seconds);
-			        	// need to cap it at 30 secs
-			        if (--timer < 0) {
-			        	// alert ("Sorry out of time! Player 2, you're up!")
-			        	if(window.prompt('Out of time! Click okay to check out the high scores on the','BOARD OF BEST PUPPY BUDDIES'))
-  location.href='https://mfullford.github.io/project1/scoreBoard.html';
-			            timer = duration;
-			        }
-			    }, 1000);
-			}
-				// 30 seconds per round
-			jQuery(function ($) {
-			    var thirtySeconds = 30 * 1,
-			        display = $('#counter');
-			    startTimer(thirtySeconds, display);
-			});
-
 
 // pseudo code for other collision detection
 
@@ -183,16 +228,17 @@ $(document).ready(function() {
  //    }
 
 
-	var highscore = localStorage.getItem("highscore");
+		// var highscore = localStorage.getItem("highscore");
 
-	if(highscore !== null){
-    if (score > highscore) {
-        localStorage.setItem("highscore", score);      
-    }
-}
-else{
-    localStorage.setItem("highscore", currentscore);
-}
+		// function scoring() {
+		// 	if(highscore !== null){
+	 //    	if (currentScore > highscore) {
+	 //        	localStorage.setItem("highscore", currentScore);      
+	 //    	}
+		// 	} else{
+		// 	    localStorage.setItem("highscore", currentScore);
+		// 	}
+		// }
 
+		// $(".scoreboard").append(highscore);
 
-});
